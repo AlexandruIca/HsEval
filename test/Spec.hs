8594a4303b3ae6d92c6de9bb688462c29c8e9527
Alexandru Ica <alexandruica703@gmail.com>
2020-12-08 11:58:50 +0200

    Remove redundant `$`

--- test/Spec.hs
+++ test/Spec.hs
@@ -4,7 +4,7 @@ import Test.Tasty
 import TokenizerTest
 
 allTests :: TestTree
-allTests = testGroup " All tests! " $ [testGroup "TokenizerTest" tokenizerTests]
+allTests = testGroup " All tests! " [testGroup "TokenizerTest" tokenizerTests]
 
 main :: IO ()
 main = do defaultMain allTests
