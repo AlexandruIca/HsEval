dd7f5e45b133f72fa84127d339804ca8ae1380e2
Alexandru Ica <alexandruica703@gmail.com>
2020-12-08 18:10:22 +0200

    Format

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,16 +1,18 @@
 module Main where
 
-import RPNTest
 import EvalPostfixTest
+import RPNTest
 import Test.Tasty
 import TokenizerTest
 
 allTests :: TestTree
-allTests = testGroup " All tests! " [
-                                    testGroup "TokenizerTest" tokenizerTests,
-                                    testGroup "RPNTest" rpnTests,
-                                    testGroup "EvalPostfixTest" evalPostfixTests
-                                    ]
+allTests =
+  testGroup
+    " All tests! "
+    [ testGroup "TokenizerTest" tokenizerTests,
+      testGroup "RPNTest" rpnTests,
+      testGroup "EvalPostfixTest" evalPostfixTests
+    ]
 
 main :: IO ()
 main = do defaultMain allTests
