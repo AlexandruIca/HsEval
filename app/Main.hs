ee5c38fba4c35a5f3cf72c4720aff0c8bbc9cfd8
Alexandru Ica <alexandruica703@gmail.com>
2020-12-08 12:03:09 +0200

    Apply stylish-haskell

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,13 @@
 module Main where
 
-import Tokenizer
 import Parser
+import Tokenizer
 
 expression :: String
 expression = "(1 + 256 - 3 / 4 * 5 + 5^(2^1))"
 
---expression :: String
---expression = "2^3 + 10 + 3 * (7 - 15 / 3)"
-
 main :: IO ()
-main = do { putStrLn $ "Lexing for: " ++ expression ++ "\n" ++ show (tokenize expression)
-          ; putStrLn $ "Postfix: " ++ show ((rpn . tokenize) expression)
-          ; putStrLn $ "Eval for " ++ expression ++ " = " ++ show (evalPostfix expression)
-          }
+main = do
+  putStrLn $ "Lexing for: " ++ expression ++ "\n" ++ show (tokenize expression)
+  putStrLn $ "Postfix: " ++ show ((rpn . tokenize) expression)
+  putStrLn $ "Eval for " ++ expression ++ " = " ++ show (evalPostfix expression)
