496593693e6004c84d4a63ecbae73925d3085412
Alexandru Ica <alexandruica703@gmail.com>
2020-12-08 11:52:49 +0200

    Use uncurry

--- test/TokenizerTest.hs
+++ test/TokenizerTest.hs
@@ -1,16 +1,15 @@
-module TokenizerTest(
-    tokenizerTests,
-) where
+module TokenizerTest
+  ( tokenizerTests,
+  )
+where
 
 import Test.Tasty
-import Test.Tasty.HUnit
-
+import Test.Tasty.HUnit ( testCase, assertBool )
 import Tokenizer
 
 tests :: [(String, [Token])]
 tests =
-    [
-    ("", []),
+  [ ("", []),
     ("  ", []),
     ("+", [Plus]),
     ("-", [Minus]),
@@ -24,20 +23,36 @@ tests =
     ("10^2", [Number 10.0, Pow, Number 2.0]),
     ("2^(3+7)", [Number 2.0, Pow, LParen, Number 3.0, Plus, Number 7.0, RParen]),
     ("2 ^ ( 3 + 7    )", [Number 2.0, Pow, LParen, Number 3.0, Plus, Number 7.0, RParen]),
-    ("1 + 256 - 3 / 4 * 5 + 5^(2^1)", [ Number 1.0, Plus, Number 256.0, Minus
-                                      , Number 3.0, Div, Number 4.0, Mult, Number 5.0, Plus
-                                      , Number 5.0, Pow, LParen, Number 2.0, Pow, Number 1.0, RParen
-                                      ])
-    ]
+    ( "1 + 256 - 3 / 4 * 5 + 5^(2^1)",
+      [ Number 1.0,
+        Plus,
+        Number 256.0,
+        Minus,
+        Number 3.0,
+        Div,
+        Number 4.0,
+        Mult,
+        Number 5.0,
+        Plus,
+        Number 5.0,
+        Pow,
+        LParen,
+        Number 2.0,
+        Pow,
+        Number 1.0,
+        RParen
+      ]
+    )
+  ]
 
 tokenizerTests :: [TestTree]
 tokenizerTests = map toTestTree tests
-    where
-        isEq :: (String, [Token]) -> Bool
-        isEq (s, tokens) = and $ map (\(a, b) -> a == b) (zip (tokenize s) tokens)
+  where
+    isEq :: (String, [Token]) -> Bool
+    isEq (s, tokens) = and $ map (\(a, b) -> a == b) (zip (tokenize s) tokens)
 
-        assertMsg :: String -> [Token] -> String
-        assertMsg s expected = "`tokenize` failed for '" ++ s ++ "'\n\t\t expected: " ++ (show expected) ++ "\n\t\t output: " ++ show (tokenize s)
+    assertMsg :: String -> [Token] -> String
+    assertMsg s expected = "`tokenize` failed for '" ++ s ++ "'\n\t\t expected: " ++ show expected ++ "\n\t\t output: " ++ show (tokenize s)
 
-        toTestTree :: (String, [Token]) -> TestTree
-        toTestTree input = testCase "" (assertBool (assertMsg (fst input) (snd input)) (isEq input))
+    toTestTree :: (String, [Token]) -> TestTree
+    toTestTree input = testCase "" (assertBool (uncurry assertMsg input) (isEq input))
