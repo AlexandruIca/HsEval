9798de34d62165e3dae7d1631a211cd5a74aa3c3
Alexandru Ica <alexandruica703@gmail.com>
2020-12-08 12:07:22 +0200

    Apply sytlish-haskell

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,16 +1,16 @@
-module Parser(
-    AST(ASTNumber, ASTOp),
+module Parser
+  ( AST (ASTNumber, ASTOp),
     rpn,
     interpret,
     evalPostfix,
-)
+  )
 where
 
 import Tokenizer
 
 data AST
-    = ASTNumber Double
-    | ASTOp AST AST Token
+  = ASTNumber Double
+  | ASTOp AST AST Token
 
 isNumber :: Token -> Bool
 isNumber (Number _) = True
@@ -58,21 +58,19 @@ type Stack = [Token]
 
 shuntingYard :: Stack -> Stack -> Stack -> Stack
 shuntingYard [] [] result = result
-shuntingYard [] (op:ops) result = shuntingYard [] ops (op:result)
-shuntingYard (tok:toks) operators result
-    | isNumber tok = shuntingYard toks operators (tok:result)
-
-    | isOperator tok = case operators of
-        [] -> shuntingYard toks (tok:operators) result
-        (op:ops) -> if ((isLeftAssoc tok) && (prec tok <= prec op)) || ((isRightAssoc tok) && (prec tok < prec op))
-                    then shuntingYard (tok:toks) ops (op:result)
-                    else shuntingYard toks (tok:operators) result
-
-    | isLParen tok = shuntingYard toks (tok:operators) result
-
-    | isRParen tok = case operators of
-        (LParen:ops) -> shuntingYard toks ops result
-        (op:ops) -> shuntingYard (tok:toks) ops (op:result)
+shuntingYard [] (op : ops) result = shuntingYard [] ops (op : result)
+shuntingYard (tok : toks) operators result
+  | isNumber tok = shuntingYard toks operators (tok : result)
+  | isOperator tok = case operators of
+    [] -> shuntingYard toks (tok : operators) result
+    (op : ops) ->
+      if ((isLeftAssoc tok) && (prec tok <= prec op)) || ((isRightAssoc tok) && (prec tok < prec op))
+        then shuntingYard (tok : toks) ops (op : result)
+        else shuntingYard toks (tok : operators) result
+  | isLParen tok = shuntingYard toks (tok : operators) result
+  | isRParen tok = case operators of
+    (LParen : ops) -> shuntingYard toks ops result
+    (op : ops) -> shuntingYard (tok : toks) ops (op : result)
 
 -- Reverse Polish Notation
 rpn :: Stack -> Stack
@@ -80,44 +78,44 @@ rpn tokens = reverse $ shuntingYard tokens [] []
 
 interpret :: [Token] -> Double
 interpret tokens = interpret' [] tokens 0
-    where
-        keepNumbers :: Token -> Bool
-        keepNumbers (Number _) = True
-        keepNumbers _ = False
+  where
+    keepNumbers :: Token -> Bool
+    keepNumbers (Number _) = True
+    keepNumbers _ = False
 
-        toDouble :: Token -> Double
-        toDouble (Number n) = n
-        toDouble _ = error $ "Didn't expect an operator in `toDouble`"
+    toDouble :: Token -> Double
+    toDouble (Number n) = n
+    toDouble _ = error $ "Didn't expect an operator in `toDouble`"
 
-        keepOperators :: Token -> Bool
-        keepOperators (Number _) = False
-        keepOperators _ = True
+    keepOperators :: Token -> Bool
+    keepOperators (Number _) = False
+    keepOperators _ = True
 
-        takeAllButLast2 :: [a] -> [a]
-        takeAllButLast2 lst = take (length lst - 2) lst
+    takeAllButLast2 :: [a] -> [a]
+    takeAllButLast2 lst = take (length lst - 2) lst
 
-        takeLast2 :: [a] -> (a, a)
-        takeLast2 lst =
-            let toConvert = reverse $ take 2 (reverse lst)
-            in (head toConvert, last toConvert)
+    takeLast2 :: [a] -> (a, a)
+    takeLast2 lst =
+      let toConvert = reverse $ take 2 (reverse lst)
+       in (head toConvert, last toConvert)
 
-        processOperation :: Token -> (Double, Double) -> Double
-        processOperation Plus (a, b) = a + b
-        processOperation Minus (a, b) = a - b
-        processOperation Mult (a, b) = a * b
-        processOperation Div (a, b) = a / b
-        processOperation Pow (a, b) = a ** b
-        processOperation _ _ = error $ "Unexpected token in `processOperation` when interpreting"
+    processOperation :: Token -> (Double, Double) -> Double
+    processOperation Plus (a, b) = a + b
+    processOperation Minus (a, b) = a - b
+    processOperation Mult (a, b) = a * b
+    processOperation Div (a, b) = a / b
+    processOperation Pow (a, b) = a ** b
+    processOperation _ _ = error $ "Unexpected token in `processOperation` when interpreting"
 
-        interpret' :: [Double] -> Stack -> Double -> Double
-        interpret' _ [] x = x
-        interpret' acc (tok:toks) result = case tok of
-            Number n -> interpret' (acc ++ [n]) toks result
-            LParen -> error $ "Unexpected '(' token in `interpret'`"
-            RParen -> error $ "Unexpected ')' token in `interpret'`"
-            _ ->
-                let res = processOperation tok (takeLast2 acc)
-                in interpret' ((takeAllButLast2 acc) ++ [res]) toks res
+    interpret' :: [Double] -> Stack -> Double -> Double
+    interpret' _ [] x = x
+    interpret' acc (tok : toks) result = case tok of
+      Number n -> interpret' (acc ++ [n]) toks result
+      LParen -> error $ "Unexpected '(' token in `interpret'`"
+      RParen -> error $ "Unexpected ')' token in `interpret'`"
+      _ ->
+        let res = processOperation tok (takeLast2 acc)
+         in interpret' ((takeAllButLast2 acc) ++ [res]) toks res
 
 evalPostfix :: String -> Double
 evalPostfix expression = interpret (rpn . tokenize $ expression)
